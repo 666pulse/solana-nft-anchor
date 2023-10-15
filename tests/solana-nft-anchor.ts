bf5018973c68a22579f0ab2544fff26a6fdca940
liushooter <shooter.hero@gmail.com>
2023-10-16 02:20:28 +0800

    test code

--- tests/solana-nft-anchor.ts
+++ tests/solana-nft-anchor.ts
@@ -1,16 +1,82 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaNftAnchor } from "../target/types/solana_nft_anchor";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import {
+  findMasterEditionPda,
+  findMetadataPda,
+  mplTokenMetadata,
+  MPL_TOKEN_METADATA_PROGRAM_ID,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { publicKey } from "@metaplex-foundation/umi";
 
-describe("solana-nft-anchor", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+import {
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
 
-  const program = anchor.workspace.SolanaNftAnchor as Program<SolanaNftAnchor>;
+describe("solana-nft-anchor", async () => {
+  // Configured the client to use the devnet cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace
+    .SolanaNftAnchor as Program<SolanaNftAnchor>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const signer = provider.wallet;
+
+  const umi = createUmi("https://api.devnet.solana.com")
+    .use(walletAdapterIdentity(signer))
+    .use(mplTokenMetadata());
+
+  const mint = anchor.web3.Keypair.generate();
+
+  // Derive the associated token address account for the mint
+  const associatedTokenAccount = await getAssociatedTokenAddress(
+    mint.publicKey,
+    signer.publicKey
+  );
+
+  // derive the metadata account
+  let metadataAccount = findMetadataPda(umi, {
+    mint: publicKey(mint.publicKey),
+  })[0];
+
+  //derive the master edition pda
+  let masterEditionAccount = findMasterEditionPda(umi, {
+    mint: publicKey(mint.publicKey),
+  })[0];
+
+  const metadata = {
+    name: "nft",
+    symbol: "nft",
+    uri: "ipfs://bafkreigld2smzepuvshjx3knmbkzbuboeb657y7nxdpfkw7eeplkwt5snu",
+  };
+
+  it("mints nft!", async () => {
+    const tx = await program.methods
+      .initNft(metadata.name, metadata.symbol, metadata.uri)
+      .accounts({
+        signer: provider.publicKey,
+        mint: mint.publicKey,
+        associatedTokenAccount,
+        metadataAccount,
+        masterEditionAccount,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        tokenMetadataProgram: MPL_TOKEN_METADATA_PROGRAM_ID,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      })
+      .signers([mint])
+      .rpc();
+
+    console.log(
+      `mint nft tx: https://explorer.solana.com/tx/${tx}?cluster=devnet`
+    );
+    console.log(
+      `minted nft: https://explorer.solana.com/address/${mint.publicKey}?cluster=devnet`
+    );
   });
 });
